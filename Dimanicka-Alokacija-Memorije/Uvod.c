ef65295f2ceb51848a8d0801f714b311de1e1437
Lana Sundic <lanasundic@gmail.com>
2025-05-15 00:25:38 +0200

    change Uvod.c

--- Dimanicka-Alokacija-Memorije/Uvod.c
+++ Dimanicka-Alokacija-Memorije/Uvod.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-//Funkcija koja alocira niz i vraca pokazivac na prvi element niza
+/*
+Dinamicka alokacija memorije u C-u je mehanizam koji omogucava programu da tokom izvodjenja(runtime) zatrazi memoriju iz heap segmenta - za razliku od staticke alokacije
+npr. lokalne promjenljive koja se vrsi u stacku i odredjuje u vrijeme kompajliranja.
 
+Kljucne f-je za dinamicku alokaciju (iz <stdlib.h>)
+1. malloc(size_t size)
+    alocira blok memorije od size bajtova.
+    ne inicijalizuje memoriju(sadrzaj je nedefinisan)
+    vraca pokazivac (void*) na pocetak alociranog bloka.
+*/
+int *arr = (int *) malloc(5 * sizeof(int));     //prostor za 5 intova
